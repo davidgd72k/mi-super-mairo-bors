3e2c2a185594e96b4a5df080e157b19ee40c02d6
davidg72k <davidgd72k@gmail.com>
2025-01-07 14:17:55 +0100

    Added more animations and jumping now stay in air an amount of time

--- game_objects/player.gd
+++ game_objects/player.gd
@@ -7,6 +7,8 @@ class_name Player extends CharacterBody2D
 @export var jump_time_to_peak: float = 1
 ## Tiempo que tardará en caer.
 @export var jump_time_to_descent: float = 0.5
+
+@export var peak_time: float = 0.5
 @export_group("Horizontal movement")
 ## Velocidad horizontal.
 @export var horizontal_speed: float = 400.0
@@ -16,19 +18,51 @@ class_name Player extends CharacterBody2D
 @onready var sprite_2d: Sprite2D = $Sprite2D
 
 # Time-based gravity vars.
-@onready var jump_velocity: float = 0
-@onready var jump_gravity: float = 0
-@onready var fall_gravity: float = 0
+var jump_velocity: float = 0
+var jump_gravity: float = 0
+var fall_gravity: float = 0
+
+var peak_timer: Timer
+
+var in_peak: bool = false
+var you_must_falling: bool = false
+var peak_height: float = 0
+var jumping: bool = false
+
 
 func _ready() -> void:
-	animation_player.play("walk")
-	redefine_jumping_vars()
+	animation_player.play("idle")
 	
+	peak_timer = Timer.new()
+	peak_timer.wait_time = peak_time
+	peak_timer.one_shot = true
+	peak_timer.timeout.connect(_peak_timer_timeout)
+	
+	
+	add_child(peak_timer)
+	
+	redefine_jumping_vars()
+
+func _process(delta: float) -> void:
+	decide_animation()
 
 func _physics_process(delta: float) -> void:
 	# Apply time-based gravity.
 	if not is_on_floor():
 		velocity.y += get_own_gravity() * delta
+		
+		if in_peak and not you_must_falling:
+			velocity.y = 0
+	elif is_on_floor():
+		you_must_falling = false
+		jumping = false
+	
+	if is_peak_height_reach() and not in_peak and not you_must_falling:
+		print("alcanzas la altura peak")
+		peak_timer.start()
+		in_peak = true
+		
+		
 
 	# Handle jumping from the ground.
 	if Input.is_action_just_pressed("b_a") and is_on_floor():
@@ -49,27 +83,48 @@ func _physics_process(delta: float) -> void:
 
 	move_and_slide()
 
+
+func _peak_timer_timeout():
+	in_peak = false
+	you_must_falling = true
+	print("a caer toca")
+
+func decide_animation():
+	if jumping:
+		animation_player.play("jump")
+	else:
+		if velocity.length() > 0:
+			animation_player.play("walk")
+		else:
+			animation_player.play("idle")
+
+		
+	pass
+
 ## Apply horizontal movement to player character.
 func horizontal_moving(direction: float, speed: float) -> float:
 	return direction * speed
 
+func is_peak_height_reach() -> bool:
+	return self.global_position.y <= peak_height
 
-func simulate_fall(fake_gravity: float):
-	velocity.y += fake_gravity
 
-
-func starting_fall():
-	simulate_fall(fall_gravity*2)
-	await self.is_on_floor()
-	
 #region Time-based jump logic.
 ## Apply jump impulse to player character.
 func jump():
+	jumping = true
+	peak_height = get_peak_height()
 	velocity.y = jump_velocity
 
 ## Get gravity based on timing (both in jumping and falling.
 func get_own_gravity() -> float:
-	return jump_gravity if velocity.y < 0.0 else fall_gravity
+	var own_gravity = jump_gravity if velocity.y < 0.0 else fall_gravity
+	return own_gravity
+
+func get_peak_height() -> float:
+	var old_height: float = self.global_position.y
+	# HACK: cambia ese número mágico por una variable export.
+	return old_height - jump_height
 
 ## Redefine variables used for time-based gravity 
 ## (recommended call this for any change in him implied variables).
